fded6f63ac010c1919611b8f10738a2efae06d58
Bitmun <pashabalko21@gmail.com>
2024-04-16 14:34:49 +0300

    init: finish settings

--- srv/src/app.module.ts
+++ srv/src/app.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UsersModule} from "./users/users.module";
-
-const pg = new URL(process.env.APP_PG_URL);
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: pg.hostname,
-      port: parseInt(pg.port),
-      username: pg.username,
-      password: pg.password,
-      database: pg.pathname.slice(1),
-      ssl: pg.searchParams.get('sslmode') !== 'disable',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'hh-testdb',
+      ssl: false,
       autoLoadEntities: true,
       // it is unsafe to use synchronize: true for schema synchronization on production
       synchronize: false, // process.env.NODE_ENV === 'development',
--- srv/typeorm.config.ts
+++ srv/typeorm.config.ts
@@ -2,13 +2,13 @@ import * as path from 'path';
 import { DataSource } from 'typeorm';
 
 const dataSource = new DataSource({
-  url: process.env.APP_PG_URL,
+  url: 'postgresql://postgres:postgres@localhost:5432/hh-testdb?sslmode=disable',
   ssl: false,
   type: 'postgres',
   entities: [path.join('dist', '**', '*.entity.{ts,js}')],
   migrations: [path.join('./migrations/*.ts')],
   synchronize: false,
-  logging: process.env.NODE_ENV === 'development'
+  logging: process.env.NODE_ENV === 'development',
 });
 
 export default dataSource;
